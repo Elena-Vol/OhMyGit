3b5b9572c09e136ed8127b0bb264d0e25c2099ba
Elena V <vologdi@yansex.ru>
2023-01-17 14:15:47 +0300

    Task 010 средняя цифра в трехзначном

---
+++ Task010/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  Напишите программу, которая принимает на вход трёхзначное число 
+ и на выходе показывает вторую цифру этого числа. 
+ Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetNumber (int num)
+{
+    int tenth = num / 10;
+    int digit = tenth % 10;
+    return digit;
+}
+
+Console.Write("Введите трехзначное число ");
+int numder = Convert.ToInt32(Console.ReadLine());
+
+if (numder > 99 && numder <1000)
+{
+    int midlDigit = GetNumber (numder);
+Console.Write(midlDigit);
+}
+else Console.Write("Введено не корректное число");
