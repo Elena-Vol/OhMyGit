0bf154c573b3cc06fa3b2ed558fc81e10e144f1f
Elena V <vologdi@yansex.ru>
2023-01-22 16:16:14 +0300

    Example013_For_to_For

---
+++ Example013_For_to_For/Program.cs
@@ -0,0 +1,10 @@
+﻿// Цикл в цикле (for to for)
+
+for (int i = 2; i<= 10; i++)
+{
+    for (int j = 2; j<= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
