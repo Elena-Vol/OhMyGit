753a1eba557e39f783061ec0a560820544ee3caa
Elena V <vologdi@yansex.ru>
2023-01-29 14:27:16 +0300

    Example019_Fibonacci

--- Example019/Program.cs
+++ Example019/Program.cs
@@ -7,10 +7,10 @@
 double Fibonacci (int n) 
 {
     if (n == 1 || n == 2) return 1;
-    else return Fibonacci (n - 1) + Fibonacci (n - 2);
+    else return Fibonacci (n-1) + Fibonacci (n-2);
 }
 
-for (int i = 0; i < 5; i++)
+for (int i = 1; i < 10; i++)
 {
     Console.WriteLine(Fibonacci(i));
     //Console.WriteLine($"{i} = {Factirial (i)}");
