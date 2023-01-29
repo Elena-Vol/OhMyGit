16ec60b477337299e41eca89118ad38275ed81d4
Elena V <vologdi@yansex.ru>
2023-01-29 14:06:37 +0300

    Example018_Factirial

---
+++ Example018/Program.cs
@@ -0,0 +1,13 @@
+﻿// Факториал и рекурсия
+
+double Factirial (int n) // из-за переполнения уже на 17!
+{
+    if (n == 1 || n == 0) return 1;
+    else return n * Factirial (n - 1);
+}
+
+
+for (int i = 0; i < 30; i++)
+{
+    Console.WriteLine($"{i}! = {Factirial (i)}");
+}
