3cb03cd04fd1406c186f575c56900b9bfae53226
Elena V <vologdi@yansex.ru>
2023-01-29 14:13:33 +0300

    Example018_Factorial

--- Example018/Program.cs
+++ Example018/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Факториал и рекурсия
 
-double Factirial (int n) // из-за переполнения уже на 17!
+double Factorial (int n) // из-за переполнения уже на 17!
 {
     if (n == 1 || n == 0) return 1;
-    else return n * Factirial (n - 1);
+    else return n * Factorial (n - 1);
 }
 
 
-for (int i = 0; i < 30; i++)
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine($"{i}! = {Factirial (i)}");
+    Console.WriteLine($"{i}! = {Factorial (i)}");
 }
