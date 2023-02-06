5d34e0987ffb21a8dbad4cf93a85b3fc4482969a
Elena V <Elena V>
2023-02-06 19:41:40 +0300

    Example024_все сочетания из Т букв в тексте

---
+++ Example024/Program.cs
@@ -0,0 +1,18 @@
+﻿// В некотором машинном алфавите имеются четыре буквы "а", "и", "с" и "в".
+// Покажите все сочетания из Т букв, которые можно построить из букв этого алфавита.
+
+int n = 0;
+void FindWords(string alphavit, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphavit.Length; i++)
+    {
+        word[length] = alphavit[i];
+        FindWords(alphavit, word, length + 1);
+    }
+}
+
+FindWords("аисв", new char[3]);
